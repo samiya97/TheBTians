64456b4771b66fa4e4a1bb20dbfe4731db5c9a00
akramah86 <akramah86@gmail.com>
2022-10-15 00:28:10 -0700

    add core

---
+++ TheBTains/CareProWeb/Program.cs
@@ -0,0 +1,36 @@
+using CareProWeb.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.MapRazorPages();
+app.MapHub<NotificationHub>("/notification");
+
+app.Run();
